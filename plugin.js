af1c94b336a9d2848647fb529c2f2ac554b38a04
Brett Zamir <brettz9@yahoo.com>
2015-06-06 08:11:25 -0700

    Further work attempts on tinymce+codemirror

--- plugin.js
+++ plugin.js
@@ -1,55 +1,120 @@
-/*global tinymce:true */
+/*global tinymce, CodeMirror */
+/*jslint vars:true*/
 
 (function () {'use strict';
 
-tinymce.PluginManager.requireLangPack('codemirrorinline');
-
-var codeMirrorScripts = [
-    'lib/codemirror.js',
-    'addon/edit/matchbrackets.js',
-    'mode/xml/xml.js',
-    'mode/javascript/javascript.js',
-    'mode/css/css.js',
-    'mode/htmlmixed/htmlmixed.js',
-    'addon/dialog/dialog.js',
-    'addon/search/searchcursor.js',
-    'addon/search/search.js',
-    'addon/selection/active-line.js'
-].map(function (file) {
-    return '../node_modules/codemirror/' + file;
-});
-
-var ready = false;
-var scriptLoader = new tinymce.dom.ScriptLoader();
-scriptLoader.loadScripts(
-    codeMirrorScripts.slice(0, 1), // Must be available first
-    function () {
-        var scriptLoader = new tinymce.dom.ScriptLoader();
-        scriptLoader.loadScripts(
-            codeMirrorScripts.slice(1),
-            function () {
-                ready = true;
-            }
-        );
-    }
-);
-
+//tinymce.PluginManager.requireLangPack('codemirrorinline');
 
 tinymce.PluginManager.add('codemirrorinline', function (editor/*, url*/) {
-	function showSourceEditor() {
+    var ready = false;
+
+    function addCodeMirrorBase (file) {
+        return (editor.getParam('codemirrorInlinePath') || '../node_modules/codemirror/') + file;
+    }
+
+    var codemirrorStylesheets = (editor.getParam('codemirrorInlineCSS') || [// Default CSS files
+        'lib/codemirror.css',
+        'addon/dialog/dialog.css'
+    ]).map(addCodeMirrorBase)
+
+    var codemirrorScripts = (editor.getParam('codemirrorInlineJS') || [
+        'lib/codemirror.js',
+        'addon/edit/matchbrackets.js',
+        'mode/xml/xml.js',
+        'mode/javascript/javascript.js',
+        'mode/css/css.js',
+        'mode/htmlmixed/htmlmixed.js',
+        'addon/dialog/dialog.js',
+        'addon/search/searchcursor.js',
+        'addon/search/search.js',
+        'addon/selection/active-line.js'
+    ]).map(addCodeMirrorBase);
+
+
+    var i = 0;
+    function loadAllStylesheets (cb) {
+        tinymce.DOM.styleSheetLoader.load(
+            codemirrorStylesheets[i],
+            (i < codemirrorStylesheets.length - 1 ? function () {
+                loadAllStylesheets(cb);
+            } : cb)
+        );
+        i++;
+    }
+    loadAllStylesheets();
+
+    var j = 0;
+    function loadAllScripts (cb) {
+        var scriptLoader = new tinymce.dom.ScriptLoader();
+        scriptLoader.loadScripts(
+            [codemirrorScripts[j]],
+            (j < codemirrorScripts.length - 1 ? function () {
+                loadAllScripts(cb);
+            } : cb)
+        );
+        j++;
+    }
+    
+    loadAllScripts(
+        function () {
+            ready = true;
+                        
+            
+            /*CodeMirror(function(elt) {
+                var textarea = document.querySelector('#a');
+                textarea.parentNode.replaceChild(elt, textarea);
+            }, {value: '<u>abc</u>'});*/
+            editor.iframeElement.style.display = 'none';
+            var textarea = document.createElement('textarea');
+            
+            var cont = editor.iframeElement;
+            //var cont = document.body;
+            cont.parentNode.appendChild(textarea);
+            // textarea.style.all = 'unset';
+
+            var codemirror = CodeMirror.fromTextArea(textarea, {// Default config
+                mode: 'htmlmixed',
+                lineNumbers: true,
+                lineWrapping: true,
+                indentUnit: 1,
+                tabSize: 1,
+                matchBrackets: true,
+                styleActiveLine: true
+            });
+            
+            // textarea.nextElementSibling.style.all = 'initial';
+            // editor.iframeElement.parentNode.appendChild(textarea.nextElementSibling);
+            // editor.iframeElement.parentNode.insertBefore(textarea, editor.iframeElement.parentNode.lastElementChild);
+            
+            codemirror.isDirty = false;
+            codemirror.on('change', function(inst) {
+                inst.isDirty = true;
+            });
+            
+        }
+    );
+    
+    editor.on('change', function (e) {
+        if (!ready) {
+            alert('Please wait until the source code plugin has time to load (2)');
+            return;
+        }
+        
+//  alert(editor.getContent());
+//        editor.setContent('<i>italics</i>');
+        
+    });
+    
+    function showSourceEditor() {
         if (!ready) {
             alert('Please wait until the source code plugin has time to load');
             return;
         }
 		
-        //alert(CodeMirror);
-        // alert(document.querySelectorAll('.mce-edit-area')[0].firstElementChild.contentDocument.documentElement.outerHTML);
-        document.querySelectorAll('.mce-edit-area')[0].firstElementChild.style.visibility = 'hidden';
-        
         
         
 	}
-    
+
     
 	// Add a button to the button bar
 	editor.addButton('codemirrorinline', {
