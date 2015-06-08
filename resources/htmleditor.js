5e2ff0f1bd4604c7c03efe5d90342d5dcc977b44
Brett Zamir <brettz9@yahoo.com>
2015-06-07 22:04:02 -0700

    Add styles.css for our own styles; make CodeMirror auto-height; add toolbar (currently theme changing only, settable by "theme" param, localStorage, and menu); add date to cache manifest to force updates

--- resources/htmleditor.js
+++ resources/htmleditor.js
@@ -6,6 +6,7 @@
 
 document.title = 'HTML Editor';
 
+
 var pane = $('div.split-pane').children('.split-pane-divider');
 
 // Remove if https://github.com/shagstrom/split-pane/issues/14 implemented.
@@ -51,7 +52,8 @@ texteditor.value = [
 
 var ed = CodeMirror.fromTextArea(texteditor, {
     lineNumbers: true,
-    mode: 'htmlmixed'
+    mode: 'htmlmixed',
+    viewportMargin: Infinity
 });
 function setPreview () {
     // document.querySelector('#preview').innerHTML = ed.getValue(); // innerHTML won't execute scripts, so we use jQuery
@@ -63,7 +65,6 @@ setPreview();
 ed.on('change', setPreview);
 // ed.setSize(200, 200);
 
-
 // Trigger events needed to push down the splitter!
 var verticalDrop = parseFloat(localStorage.getItem('splitterTop')) || 0;
 triggerSplitterDrop(verticalDrop);
@@ -71,6 +72,36 @@ triggerSplitterDrop(verticalDrop);
 $('div.split-pane').mouseup(function () {
     localStorage.setItem('splitterTop', pane.offset().top);
 });
+
+
+// TOOLBAR
+// Theme
+var select = document.createElement('select');
+var themePrefix = 'bower_components/codemirror/theme/';
+var themeSuffix = '.css';
+Array.from(document.querySelectorAll('link')).filter(function (link) {
+    return link.href.indexOf(themePrefix) > -1;
+}).forEach(function (link) {
+    var option = document.createElement('option');
+    option.text = link.getAttribute('href').replace(themePrefix, '').replace(themeSuffix, '');
+    select.appendChild(option);
+});
+document.querySelector('#toolbar').appendChild(select);
+select.addEventListener('change', function (e) {
+    var theme = select.selectedOptions[0].value;
+    localStorage.setItem('theme', theme);
+    // ed.setOption('theme', theme);
+    // We change the location so the user can bookmark it (we might instead try parsing after the hash to prevent need for refresh)
+    var url = new URL(document.location.href);
+    url.searchParams.set('theme', theme);
+    window.location = url;
+});
+
+var choice = new URL(document.location.href).searchParams.get('theme') || localStorage.getItem('theme'); // FF doesn't currently support document.location.searchParams
+if (choice) {
+    select.value = choice;
+    ed.setOption('theme', choice);
+}
 
 
 }());
